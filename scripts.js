7f23b6c11c7a8d30c8ee9d784cb5dfff2c27e7b4
kishore <kishoresukant@gmail.com>
2020-12-02 22:23:39 +0530

    modal and form added

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
-$(document).ready(function(){
-    new WOW().init();
-});
+window.onscroll = function() {
+    scrollnav()
+};
+
+function scrollnav(){
+    if ( document.documentElement.scrollTop > 300 ){
+        document.getElementById("nav").style.padding =" 0 1rem";
+        document.getElementById("nav").style.backgroundColor = "black";
+    }
+    else {
+        document.getElementById("nav").style.padding= "0.5rem 1rem";
+        document.getElementById("nav").style.backgroundColor = "rgb(31, 26, 26,0.8)";
+    }
+
+}
